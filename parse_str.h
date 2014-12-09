5da269a8aaf60770a7097592117e344667073880
lelandbatey <lelandbatey@gmail.com>
2014-12-09 14:24:22 -0800

    Removed unused class from parse_str.h

--- parse_str.h
+++ parse_str.h
@@ -10,53 +10,9 @@
 
 using namespace std;
 
-
-// A wrapper around a string to allow for easy retrieval of single characters.
-class ParseStr
-{
-public:
-	ParseStr(string input):_str(input), _pos(0), _eof(false){};
-
-	char get(){
-		char cc;
-
-		if (_pos < _str.size()){
-			cc = _str[_pos];
-			++_pos;
-		} else {
-			cc = '\0';
-			_eof = true;
-		}
-		return cc;
-	}
-
-	bool eof(){
-		if (_pos == _str.size()){
-			_eof = true;
-		}
-		return _eof;
-	}
-
-	void dump(){
-		if (_DEBUG_){
-			for (unsigned int i = 0; i < _pos; ++i){
-				std::cout << _str[i];
-			}
-			std::cout << std::endl;
-		}
-	}
-
-	void shift_back(unsigned int back){
-		_pos -= back;
-	}
-
-
-private:
-	string _str;
-	unsigned int _pos;
-	bool _eof;
-	
-};
+// Contains all my lazy functions for string manipulation. This *cannot* be a
+// good or wise thing to do, but I don't know what I'm supposed to do with
+// this...
 
 std::string dubToStr(double dub) {
     std::ostringstream temp;
